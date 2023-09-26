026174b649c6c4b36324ad0448cf10cf87dabdc9
humbertovenavente <jnajar@unis.edu.gt>
2023-09-26 14:43:33 -0600

    nuevos cambios

--- proyecto/src/Front/Login.js
+++ proyecto/src/Front/Login.js
@@ -9,26 +9,88 @@ export const Login = () => {
     return (
 
 <div className="login" >
-        
+<body class="bg-info d-flex justify-content-center
+                align-items-center vh-100">
+      <div class="bg-white p-5 rounded-5 text-secondary" style={{width:'27rem'}} >
+        <div>
+            <div class= "d-flex justify-content-center">
+                <img src= "proyecto/src/img/persona.png"
+                alt="login-icon"
+                style={{height:'7rem'}}
+            
+                />
+
+            </div>
+
+                  
         <form id="form_login">
+           
             <div>
-                <h1>Login</h1>
+                <h1 class= "text-center fs-1">Login</h1>
                 <br></br>
-                <label>Usuario</label>
-                <input type="text" id="txtusuario" style={{textAlign:"center"}} className="form-control"  required/>
+           
+
+                <div class= "input-group">
+                <div class= "input-group-text bg-info">
+                    <img 
+                    src='proyecto/src/img/arrow-up-circle.svg'
+                    style={{height:'1rem'}}/>
+            
+                    </div>
+                <input class="form-control" type="text" id="txtusuario"  placeholder='Usuario'  required/>
+                </div>
             </div>
             
-            <div>
-                <label>Contraseña</label>
-                <input type="password" id="txtcontraseña" style={{textAlign:"center"}} className="form-control"  required/>
+            <div class= "input-group mt-1">
+                <div class= "input-group-text bg-info">
+                    <img 
+                    src='proyecto/src/img/arrow-up-circle.svg'
+                    style={{height:'1rem'}}/>
+                    </div>
+                   
+                <input class="form-control" type="password" id="txtcontraseña"  placeholder='Contraseña'  required/>
+            
+           
             </div>
+            
+
+         
+            <div class= "d-flex justify-content-around">
+                <div class= "d-flex align-items-center gap-1">
+                    <input type="checkbox"/>
+                    <div styles="font-size: 0.9rem"> Remember me</div>
+                    
+                </div>
+                <div> 
+               <a href="#" class= "text-decoration-none text-info fw-semibold fst-italic"> Forgot password?</a>
+               </div>
+            </div>
+           
+
+        
             <br></br>
-            <input type="submit"  className="btn btn-primary" value="Entrar"/>
-        </form>
+            <div class="btn btn-info text-white w-100 mt-3">
+            <input type="submit"  className="btn btn-info text-white w-100" value="Entrar"/>
+            </div>
+            <div styles="font-size: 0.9rem"  class= "d-flex align-items-center gap-1 mt-3"> 
+                Dont you have an account?
+
+                <a href="#" class= "text-decoration-none text-info fw-semibold fst-italic"> Create an account</a>
+            </div>
+            </form>
+            </div>
+            </div>
+            </body>
+            
+        
         </div>
+        
+
+        
         
     )
 }
+
 
 
 
