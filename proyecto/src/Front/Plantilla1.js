bea313f3b6e259261e2aa5dc8f17df7f8ed41e58
Gregorio0505 <josegregoriocoronelcolombo@gmail.com>
2023-09-26 09:34:37 -0600

    Plantilla 1

--- proyecto/src/Front/Plantilla1.js
+++ proyecto/src/Front/Plantilla1.js
@@ -4,6 +4,24 @@ const Plantilla1 = () => {
   return (
     <div>
       <label>Esta es la plantilla 1</label>
+
+      <h1>*Titulo del articulo*</h1>
+
+      <p style={{textAlign: "center", width: "50%"}}>
+        Esto es un texto de ejemplo. Esto es un texto de ejemplo. Esto es un texto de ejemplo. Esto es un texto de ejemplo. 
+        Esto es un texto de ejemplo. Esto es un texto de ejemplo. Esto es un texto de ejemplo. Esto es un texto de ejemplo. 
+        Esto es un texto de ejemplo. Esto es un texto de ejemplo. Esto es un texto de ejemplo. Esto es un texto de ejemplo. 
+        Esto es un texto de ejemplo. Esto es un texto de ejemplo. Esto es un texto de ejemplo. Esto es un texto de ejemplo. 
+        Esto es un texto de ejemplo. Esto es un texto de ejemplo. Esto es un texto de ejemplo. Esto es un texto de ejemplo. 
+        Esto es un texto de ejemplo. Esto es un texto de ejemplo. Esto es un texto de ejemplo. Esto es un texto de ejemplo. 
+        Esto es un texto de ejemplo. Esto es un texto de ejemplo. Esto es un texto de ejemplo. Esto es un texto de ejemplo. 
+        Esto es un texto de ejemplo. Esto es un texto de ejemplo. Esto es un texto de ejemplo. Esto es un texto de ejemplo. 
+        Esto es un texto de ejemplo. Esto es un texto de ejemplo. Esto es un texto de ejemplo. Esto es un texto de ejemplo. 
+        Esto es un texto de ejemplo. Esto es un texto de ejemplo. Esto es un texto de ejemplo. Esto es un texto de ejemplo. 
+      </p > 
+
+      <img src="https://static.vecteezy.com/system/resources/previews/009/784/979/non_2x/soccer-ball-icon-isolated-on-white-background-free-vector.jpg" className="img-fluid" alt="Pelota de fútbol" />
+
     </div>
   )
 }
