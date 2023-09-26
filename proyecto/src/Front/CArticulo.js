8852312176ed42822cb12b3bd0409987aa7c7eb2
Gregorio0505 <josegregoriocoronelcolombo@gmail.com>
2023-09-25 18:45:52 -0600

    Creacion de articulo

--- proyecto/src/Front/CArticulo.js
+++ proyecto/src/Front/CArticulo.js
@@ -3,12 +3,12 @@ import React from 'react'
 const CArticulo = () => {
   return (
     <div>
-        <label htmlFor="titulo">Ingrese el nombre del articulo</label>
-        <input type="text" id="titulo" name="titulo"/>
+      <label htmlFor="titulo">Ingrese el nombre del articulo</label>
+      <input type="text" id="titulo" name="titulo" />
 
-        <br></br>
+      <br></br>
 
-        <label htmlFor="categorias">Seleccione una categoria</label>
+      <label htmlFor="categorias">Seleccione una categoria</label>
       <select id="categoria" name="categoria">
         <option value="deporte">Deportes</option>
         <option value="tecnologia">Tecnologia</option>
@@ -17,9 +17,13 @@ const CArticulo = () => {
 
       <br></br>
 
+      <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
         <label htmlFor="texto">Ingrese el texto del articulo</label>
-        <input type="text" id="texto" name="texto"/>
-
+        <textarea id="story" name="story" rows="10" cols="33" style={{width: "50%"}}/>
+        
+        <input type="file" id="avatar" name="avatar" accept="image/png, image/jpeg" />
+      </div>
+      {/* <input type="text" id="texto" name="texto"/> */}
 
     </div>
   )
